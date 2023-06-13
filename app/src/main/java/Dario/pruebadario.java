70b8b36d48fa96fc1c59d7226e54e4e4d6410a59
Dario Marconi <dario.marconi@tsoftlatam.com>
2023-06-13 14:50:00 -0300

    cambios 2 de dario

---
+++ app/src/main/java/Dario/pruebadario.java
@@ -0,0 +1,20 @@
+package Dario;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class pruebadario {
+    @Test
+    void levantarBrowser() throws InterruptedException{
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://selenium.dev");
+
+        Thread.sleep(5000);
+
+        driver.close();
+
+    }
+}
